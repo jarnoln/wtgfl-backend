b24239233c0c92eee47028f457297b176d01845d
Jarno Luoma-Nirva <jarno.luoma-nirva@vaisto.io>
2021-04-06 22:25:38 +0300

    Add STATIC_ROOT setting

--- wtgflb/settings.py
+++ wtgflb/settings.py
@@ -123,6 +123,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+if not DEBUG:
+   # Tell Django to copy statics to the `staticfiles` directory
+   # in your application directory on Render.
+   STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
